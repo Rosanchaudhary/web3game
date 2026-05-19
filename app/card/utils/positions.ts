fef1557a9c4a78db3e4db672da6a54b3e2d22bed
Rosanchaudhary <chaudharyroshan2020@gmail.com>
2026-05-19 11:35:12 +0545

    rm:others

--- app/card/utils/positions.ts
+++
@@ -1,7 +0,0 @@
-//app/card/utils/positions.ts
-export const PLAYER_POSITIONS = {
-    A: { x: 0, y: 140 },
-    B: { x: -150, y: 0 },
-    C: { x: 0, y: -140 },
-    D: { x: 150, y: 0 },
-} as const;
